1729898d149fd55ece5c2c650d3f30578d05571a
Dexter Hedman <dexterhedman05@proton.me>
2025-04-28 14:46:23 +0200

    Added 'addBuffer' function to BufferManager

--- src/bufferManager.zig
+++ src/bufferManager.zig
@@ -5,15 +5,18 @@ const Buffer = buffer_mod.Buffer;
 
 pub const BufferManager = struct {
     buffers: []Buffer,
+    buffer_count: usize,
     index: usize,
 
     pub fn init(allocator: std.mem.Allocator) !BufferManager {
         const scratch = try Buffer.initScratch(allocator);
         var buffers = try allocator.alloc(Buffer, 1);
+        const buffer_count = 1;
         buffers[0] = scratch;
 
         return BufferManager{
             .buffers = buffers,
+            .buffer_count = buffer_count,
             .index = 0,
         };
     }
@@ -28,5 +31,18 @@ pub const BufferManager = struct {
 
     pub fn getActiveBuffer(self: *const BufferManager) *Buffer {
         return &self.buffers[self.index];
+    }
+
+    pub fn addBuffer(self: *BufferManager, allocator: std.mem.Allocator, buffer: Buffer) !void {
+        if (self.buffer_count + 1 > self.buffers.len) {
+            const buffers = try allocator.alloc(Buffer, self.buffer_count * 2);
+            @memcpy(buffers[0..self.buffers.len], self.buffers);
+
+            allocator.free(self.buffers);
+            self.buffers = buffers;
+        }
+
+        self.buffers[self.buffer_count] = buffer;
+        self.buffer_count += 1;
     }
 };
--- src/main.zig
+++ src/main.zig
@@ -8,17 +8,7 @@ const Buffer = buffer_mod.Buffer;
 const BufferManager = buffer_manager_mod.BufferManager;
 const Allocator = std.mem.Allocator;
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    const manager = try BufferManager.init(allocator);
-    defer manager.deinit(allocator);
-    const stdout = std.io.getStdOut().writer();
-
-    const buffer = manager.getActiveBuffer();
-
-    try buffer.content.tree(stdout, 0);
-}
+pub fn main() !void {}
 
 test "initLeaf initializes with correct content and length" {
     const allocator = std.testing.allocator;
@@ -85,6 +75,29 @@ test "Buffer insert and delete" {
     defer allocator.free(slice);
 
     try std.testing.expectEqualStrings("Hello", slice);
+}
+
+test "BufferManager buffer adding and switching" {
+    const allocator = std.testing.allocator;
+    var manager = try BufferManager.init(allocator);
+    defer manager.deinit(allocator);
+
+    const buffer = try Buffer.init(allocator);
+    try manager.addBuffer(allocator, buffer);
+
+    try std.testing.expectEqual(2, manager.buffer_count);
+
+    manager.index += 1;
+
+    var second_buffer = manager.getActiveBuffer();
+    try second_buffer.insertLine(allocator, "Hello from the second buffer!");
+    
+    try std.testing.expectEqual(1, manager.index);
+
+    const slice = try second_buffer.content.flatten(allocator);
+    defer allocator.free(slice);
+
+    try std.testing.expectEqualStrings("Hello from the second buffer!", slice);
 }
 
 // TODO:
